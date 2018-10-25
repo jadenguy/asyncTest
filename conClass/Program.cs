bdbd98c2d5aeafcbe44066c1d92d2ecd0e563a19
Alfredo Inostroza <jadenguy@gmail.com>
2018-10-25 12:06:37 -0400

    Initial Commit

---
+++ conClass/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace conClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
